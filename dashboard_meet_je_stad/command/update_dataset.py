2e99696ab77c605ad6bef8bd91e5a91a7bb6176e
Harry de Boer <hldeboer@gmail.com>
2025-10-24 12:52:22 +0200

    live data

--- dashboard_meet_je_stad/command/update_dataset.py
+++ dashboard_meet_je_stad/command/update_dataset.py
@@ -1,60 +1,66 @@
 from dashboard_meet_je_stad.service.meet_je_stad_api_service import MeetJeStadAPIService
+from dashboard_meet_je_stad.service.utrecht_ids_service import UpdateUtrechtIdsService
 import csv
 import os
 import datetime
+import dotenv
 
-
-date = datetime.datetime.now(datetime.timezone.utc)
-sensors = []
-results_old = []
-with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/dataset.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for index, row in enumerate(reader):
-        if row[1] not in sensors:
-            sensors.append(row[1])
-        if index == 0:
-            start_date = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S").replace(tzinfo=datetime.timezone.utc)
-        end_date = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S").replace(tzinfo=datetime.timezone.utc)
-        if (datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S").replace(
-                tzinfo=datetime.timezone.utc) - date).days > -366:
-            results_old.append(row)
-csvfile.close()
-
-delta = date - end_date
-results = MeetJeStadAPIService().get_data(
-            end_date.strftime('%Y-%m-%d,%H:%M:%S'),
-            date.strftime('%Y-%m-%d,%H:%M:%S'),
-    'sensors',
-'json',
-            'Utrecht',
-        False,
-        2 * delta.days * 24 * 4 * len(sensors),
-False)
-file = open(os.path.dirname(os.getcwd()) + "/dataset.csv", "w", newline='')
-full_dataset = results_old + results
-csv.writer(file).writerows(full_dataset)
-file.close()
-
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
 ids = {}
-with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/dataset.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for index, row in enumerate(reader):
+sensor_step = 50
+last_sensor_id = int(os.getenv('LAST_SENSOR_ID'))
+end_date = datetime.datetime.strptime(os.getenv('END_DATE'),"%Y-%m-%d,%H:%M:%S").replace(tzinfo=datetime.timezone.utc)
+date_now = datetime.datetime.now(datetime.timezone.utc)
+delta = date_now - end_date
+for sensor_id_50 in range(0, int(last_sensor_id / sensor_step) + 2):
+    ids_range = str(sensor_id_50 * sensor_step + 1) + '-' + str((sensor_id_50 + 1) * sensor_step)
+    end_date += datetime.timedelta(seconds=1)
+    results = MeetJeStadAPIService().get_data(
+        end_date.strftime('%Y-%m-%d,%H:%M:%S'),
+        date_now.strftime('%Y-%m-%d,%H:%M:%S'),
+        'sensors',
+        'json',
+        ids_range,
+        False,
+        2 * delta.days * 24 * 4 * sensor_step,
+        False)
+    for row in results:
         if row[1] not in ids:
             ids[row[1]] = [row]
         else:
             ids[row[1]] += [row]
-for index, id_sensor in ids.items():
-    os.makedirs(os.path.dirname(os.getcwd()) + '/ids/' + index, exist_ok=True)
-    file = open(os.path.dirname(os.getcwd()) + "/ids/" + index + "/out.csv", "w", newline='')
-    csv.writer(file).writerows(id_sensor)
+UpdateUtrechtIdsService().update(ids, end_date.strftime('%Y-%m-%d'))
+utrecht_ids = []
+with open(os.path.dirname(os.getcwd()) + '/utrecht_ids.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for index, row in enumerate(reader):
+        utrecht_ids.append(row[0])
+
+
+for index, rows in ids.items():
+    os.makedirs(os.path.dirname(os.getcwd()) + '/ids/' + str(index), exist_ok=True)
+    file = open(os.path.dirname(os.getcwd()) + "/ids/" + str(index) + "/out.csv", "a", newline='')
+    csv.writer(file).writerows(rows)
     file.close()
+    if index > last_sensor_id:
+        last_sensor_id = index
+
+    if str(index) in utrecht_ids:
+        file = open(os.path.dirname(os.getcwd()) + "/dataset_small.csv", "a", newline='')
+        csv.writer(file).writerows(rows)
+        file.close()
 
 rows = []
-for row in full_dataset:
-    date_row = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S").replace(tzinfo=datetime.timezone.utc)
-    if date - date_row < datetime.timedelta(hours=48):
-        rows.append(row)
-
+with open(os.path.dirname(os.getcwd()) + '/dataset_small.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for index, row in enumerate(reader):
+        date_row = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S").replace(tzinfo=datetime.timezone.utc)
+        if date_now - date_row < datetime.timedelta(hours=48):
+            rows.append(row)
 file = open(os.path.dirname(os.getcwd()) + "/dataset_small.csv", "w", newline='')
 csv.writer(file).writerows(rows)
 file.close()
+
+dotenv.set_key(dotenv_file, "LAST_SENSOR_ID", str(last_sensor_id), quote_mode='never')
+dotenv.set_key(dotenv_file, "END_DATE", date_now.strftime('%Y-%m-%d,%H:%M:%S'), quote_mode='never')
---
+++ dashboard_meet_je_stad/service/utrecht_ids_service.py
@@ -0,0 +1,99 @@
+import csv
+import datetime
+import math
+import os
+
+
+class UpdateUtrechtIdsService:
+
+    def __init__(self):
+        self.utrecht_center_lat_degrees = 52.085 * math.pi / 180
+        self.utrecht_center_long_degrees = 5.085 * math.pi / 180
+        self.radius = 9.46
+
+    def update(self, ids: dict, last_date: str):
+        rows_old = {}
+        with open(os.path.dirname(os.getcwd()) + "/utrecht_ids.csv", newline='') as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            for key, row in enumerate(reader):
+                rows_old[row[0]] = row
+        rows_utrecht = {}
+        for index, rows in ids.items():
+            latitudes = {}
+            longitudes = {}
+            utrecht = []
+            particulate_matter = 0
+            start_date = ''
+            end_date = ''
+            count_latitude = 0
+            count_longitude = 0
+            for key, row in enumerate(rows):
+                date_object = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
+                date = date_object.strftime('%Y-%m-%d')
+                end_date = date
+                if key == 0:
+                    start_date = date
+                latitude = row[4]
+                if latitude is None:
+                    continue
+                else:
+                    latitude = float(latitude)
+                longitude = row[3]
+                if longitude is None:
+                    continue
+                else:
+                    longitude = float(longitude)
+                latitudes[date] = latitude
+                if date in latitudes:
+                    count_latitude += 1
+                    latitudes[date] = (latitude + latitudes[date] * (count_latitude - 1)) / count_latitude
+                else:
+                    latitudes[date] = latitude
+                    count_latitude = 1
+                if date in longitudes:
+                    count_longitude += 1
+                    longitudes[date] = (longitude + longitudes[date] * (count_longitude - 1)) / count_longitude
+                else:
+                    longitudes[date] = longitude
+                    count_longitude = 1
+                if row[9] is not None or row[10] is not None:
+                    particulate_matter = 1
+            utrecht_city = False
+            start_date_utrecht = ''
+            end_date_utrecht = ''
+            for key, latitude in latitudes.items():
+                longitude = longitudes[key]
+                utrecht_row = [key]
+                end_date_utrecht = key
+                degrees_lat = math.pi / 180 * latitude
+                degrees_lon = math.pi / 180 * longitude
+                if longitude > 180 or longitude < -180 or latitude > 90 or latitude < -90:
+                    continue
+                distance = 2 * math.asin(math.sqrt(((1 - math.cos(degrees_lat - self.utrecht_center_lat_degrees)) +
+                                           math.cos(degrees_lat) * math.cos(self.utrecht_center_lat_degrees) *
+                                           (1 - math.cos(degrees_lon - self.utrecht_center_long_degrees))) / 2)) * 6371
+                if distance < self.radius:
+                    utrecht_row.append(1)
+                    if not utrecht_city and start_date_utrecht == '':
+                        start_date_utrecht = key
+                        utrecht_city = True
+                    else:
+                        utrecht_row.append(0)
+                        utrecht_city = False
+                    utrecht.append(utrecht_row)
+            if utrecht_city or start_date_utrecht != '':
+                if end_date == last_date:
+                    end_date = ''
+                if end_date_utrecht == last_date:
+                    end_date_utrecht = ''
+                rows_utrecht[index] = [index, start_date, end_date,
+                                       start_date_utrecht, end_date_utrecht, particulate_matter]
+
+        rows_new = []
+        for index, row in rows_utrecht.items():
+            rows_old[str(index)] = row
+        for index, row in rows_old.items():
+            rows_new.append(row)
+        file = open(os.path.dirname(os.getcwd()) + "/utrecht_ids.csv", "w", newline='')
+        csv.writer(file).writerows(rows_new)
+        file.close()
