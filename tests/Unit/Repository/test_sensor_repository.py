1c63ec5d9acb12f6cd3c9b1df4ad40499f8dfbdf
Harry de Boer <hldeboer@gmail.com>
2025-11-27 10:46:54 +0100

    tests update

--- tests/Unit/Repository/test_sensor_repository.py
+++ tests/Unit/Repository/test_sensor_repository.py
@@ -7,5 +7,5 @@ class TestSensorRepository(unittest.TestCase):
     def test_get_data(self) -> None:
         repository = SensorRepository()
 
-        result = repository.get(False)
+        result = repository.find_all(False)
         self.assertEqual(len(result), 1)
