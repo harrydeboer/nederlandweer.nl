862fd2e6811126ab2e83c678d0a9eccea29cf0c3
Harry de Boer <hldeboer@gmail.com>
2026-01-17 13:04:56 +0100

    years from dotenv in tests

--- tests/Unit/Service/test_curve_service.py
+++ tests/Unit/Service/test_curve_service.py
@@ -1,14 +1,20 @@
 import unittest
 from nederland_weer.service.curve_service import CurveService
+from dotenv import load_dotenv
+import os
 
 
 class TestCurveService(unittest.TestCase):
 
     def testFindAll(self) -> None:
+        load_dotenv()
+        first_year = int(os.getenv('BEGIN_YEAR'))
+        last_year = int(os.getenv('END_YEAR'))
+        begin_year_rain_perc = int(os.getenv('BEGIN_YEAR_RAIN_PERCENTAGE'))
         (json_data, title,
          vertical, horizontal, text_output) = CurveService().make_curve('temperature-day',
-                                                                                        1906, 2025,
-                                                                                        2025, 1930)
+                                                                                        first_year, last_year,
+                                                                                        last_year, begin_year_rain_perc)
         self.assertIsInstance(json_data, str)
         self.assertIsInstance(title, str)
         self.assertIsInstance(vertical, str)
