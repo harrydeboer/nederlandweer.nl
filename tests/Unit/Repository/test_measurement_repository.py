5e6d126096bf1a174d63853ef03bebc2abb00dda
Harry de Boer <hldeboer@gmail.com>
2026-01-17 12:30:21 +0100

    tests improved

--- tests/Unit/Repository/test_measurement_repository.py
+++ tests/Unit/Repository/test_measurement_repository.py
@@ -1,14 +1,12 @@
 import unittest
+import numpy as np
 from nederland_weer.repository.measurement_repository import MeasurementRepository
 
 
 class TestMeasurementRepository(unittest.TestCase):
 
     def testFindAll(self) -> None:
-
-        first_year = 1906
-        last_year = 2025
         temp_array = MeasurementRepository().find_all()
 
-        self.assertEqual(1, 1)
+        self.assertIsInstance(temp_array, np.ndarray)
 #
--- tests/Unit/Service/test_curve_service.py
+++ tests/Unit/Service/test_curve_service.py
@@ -5,4 +5,12 @@ from nederland_weer.service.curve_service import CurveService
 class TestCurveService(unittest.TestCase):
 
     def testFindAll(self) -> None:
-        self.assertEqual(1, 1)
+        (json_data, title,
+         vertical, horizontal, text_output) = CurveService().make_curve('temperature-day',
+                                                                                        1906, 2025,
+                                                                                        2025, 1930)
+        self.assertIsInstance(json_data, str)
+        self.assertIsInstance(title, str)
+        self.assertIsInstance(vertical, str)
+        self.assertIsInstance(horizontal, str)
+        self.assertIsInstance(text_output, str)
