f0744456dfaeb63cb7a810ad481280680741ccf7
Harry de Boer <hldeboer@gmail.com>
2026-01-20 22:46:48 +0100

    first and last year updated by update dataset command

--- nederland_weer/management/commands/update_dataset.py
+++ nederland_weer/management/commands/update_dataset.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from nederland_weer.repository.measurement_repository import MeasurementRepository
 import urllib.request
 import zipfile
 import os
@@ -15,11 +16,18 @@ class Command(BaseCommand):
             reader = csv.reader(input_file)
             for row in reader:
                 stations.append(row)
+        path_project = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-        for station in stations:
-            path_project = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+        for index, station in enumerate(stations):
             urllib.request.urlretrieve("https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/daggegevens/etmgeg_"
                                        + station[0] + ".zip", 'data/test.zip')
             with zipfile.ZipFile(path_project + '/data/test.zip', 'r') as zip_ref:
                 zip_ref.extractall(path_project + '/data')
             pathlib.Path.unlink(path_project + '/data/test.zip')
+            measurements = MeasurementRepository().find_all(int(station[0]))
+            stations[index][2] = measurements[0][1][:4]
+            stations[index][3] = measurements[-1][1][:4]
+
+        file = open(path_project + "/data/station.csv", "w", newline='')
+        csv.writer(file).writerows(stations)
+        file.close()
