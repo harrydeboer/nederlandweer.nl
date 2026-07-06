4c23bb8f5dca18e377d7a04b4bf7b216eb63770e
Harry de Boer <hldeboer@gmail.com>
2026-07-06 17:52:08 +0200

    update dataset improved

--- dashboard_meet_je_stad/management/commands/update_dataset.py
+++ dashboard_meet_je_stad/management/commands/update_dataset.py
@@ -3,6 +3,7 @@ from dashboard_meet_je_stad.repository.measurement_repository import Measurement
 from dashboard_meet_je_stad.repository.sensor_repository import SensorRepository
 from django.core.management.base import BaseCommand
 import os
+import math
 import datetime
 import dotenv
 from dashboard_meet_je_stad.models import Sensor
@@ -16,7 +17,6 @@ class Command(BaseCommand):
         dotenv.load_dotenv(dotenv_file)
         measurement_repository = MeasurementRepository()
         sensor_repository = SensorRepository()
-        sensor_step = 50
         last_sensor_id = int(os.getenv('LAST_SENSOR_ID'))
         if os.getenv('END_DATE') == '':
             end_date = (datetime.datetime.strptime(os.getenv('START_DATE'), '%Y-%m-%d,%H:%M:%S')
@@ -27,6 +27,10 @@ class Command(BaseCommand):
             end_date += datetime.timedelta(seconds=1)
         date_now = datetime.datetime.now(datetime.timezone.utc)
         delta = date_now - end_date
+        sensor_step = math.ceil(50 / (delta.days + 1) * 7)
+        sensors = sensor_repository.find_all()
+        first_measurements = {}
+        last_measurements = {}
         for sensor_id_50 in range(0, int(last_sensor_id / sensor_step) + 2):
             ids_range = str(sensor_id_50 * sensor_step + 1) + '-' + str((sensor_id_50 + 1) * sensor_step)
             results = MeetJeStadAPIService().get_data(
@@ -36,14 +40,11 @@ class Command(BaseCommand):
         'json',
                 ids_range,
         False,
-                2 * (delta.days + 1) * 24 * 4 * sensor_step,
+                (delta.days + 1) * 24 * 4 * sensor_step,
         False)
             if len(results) == 0:
                 continue
             measurements = []
-            sensors = sensor_repository.find_all()
-            first_measurements = {}
-            last_measurements = {}
             for row in results:
                 measurement = measurement_repository.row_to_measurement(row)
                 if measurement.is_in_utrecht() and measurement.sensor_id not in sensors:
@@ -66,17 +67,16 @@ class Command(BaseCommand):
                     measurements.append(measurement)
                     last_measurements[sensor.id] = measurement
             measurement_repository.bulk_create(measurements)
-            sensors = sensor_repository.find_all()
-            for sensor_id, sensor in sensors.items():
-                if sensor_id in first_measurements:
-                    sensor.first_measurement = measurement_repository.get_by_sensor_and_timestamp(sensor_id,
-                                                                                first_measurements[
-                                                                                    sensor_id].timestamp).id
-                if sensor_id in last_measurements:
-                    sensor.last_measurement = measurement_repository.get_by_sensor_and_timestamp(sensor_id,
-                                                                               last_measurements[
-                                                                                   sensor_id].timestamp).id
-                sensor_repository.update(sensor)
 
+        for sensor_id, sensor in sensors.items():
+            if sensor_id in first_measurements:
+                sensor.first_measurement = (measurement_repository.
+                                            get_by_sensor_and_timestamp(sensor_id,
+                                                                        first_measurements[sensor_id].timestamp).id)
+            if sensor_id in last_measurements:
+                sensor.last_measurement = (measurement_repository.
+                                           get_by_sensor_and_timestamp(sensor_id,
+                                                                       last_measurements[sensor_id].timestamp).id)
+            sensor_repository.update(sensor)
         dotenv.set_key(dotenv_file, "LAST_SENSOR_ID", str(last_sensor_id), quote_mode='never')
         dotenv.set_key(dotenv_file, "END_DATE", date_now.strftime('%Y-%m-%d,%H:%M:%S'), quote_mode='never')
