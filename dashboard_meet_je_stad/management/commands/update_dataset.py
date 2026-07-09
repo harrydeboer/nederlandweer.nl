040476e84aa2e14e454014999ba0d27d1f5a558a
Harry de Boer <hldeboer@gmail.com>
2026-07-09 15:58:46 +0200

    update_dataset improved

--- dashboard_meet_je_stad/management/commands/update_dataset.py
+++ dashboard_meet_je_stad/management/commands/update_dataset.py
@@ -7,6 +7,7 @@ import os
 import math
 import datetime
 import dotenv
+import sys
 from dashboard_meet_je_stad.models import Sensor, Measurement
 
 
@@ -16,10 +17,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dotenv_file = dotenv.find_dotenv()
         dotenv.load_dotenv(dotenv_file)
+
         measurement_repository = MeasurementRepository()
         measurement_cached_repository = MeasurementCachedRepository()
         sensor_repository = SensorRepository()
+
         sensors = sensor_repository.find_all()
+
         measurements_cached_dict = measurement_cached_repository.find_all(sensors)
         measurements_cached = []
         last_measurements = {}
@@ -27,21 +31,37 @@ class Command(BaseCommand):
             for measurement in measurements:
                 measurements_cached.append(measurement)
                 last_measurements[measurement.sensor_id] = measurement
-        last_sensor_id = int(os.getenv('LAST_SENSOR_ID'))
-        if os.getenv('END_DATE') == '':
-            end_date = (datetime.datetime.strptime(os.getenv('START_DATE'), '%Y-%m-%d,%H:%M:%S')
+
+        last_sensor_id = os.getenv('LAST_SENSOR_ID')
+        if last_sensor_id is not None and last_sensor_id != '':
+            last_sensor_id = int(last_sensor_id)
+        else:
+            self.stdout.write(self.style.ERROR('Last sensor missing in .env.'))
+            sys.exit(1)
+
+        end_date = os.getenv('END_DATE')
+        if end_date == '':
+            start_date = os.getenv('START_DATE')
+            if start_date is None or start_date == '':
+                self.stdout.write(self.style.ERROR('Start date missing in .env.'))
+                sys.exit(1)
+            end_date = (datetime.datetime.strptime(start_date, '%Y-%m-%d,%H:%M:%S')
                         .replace(tzinfo=datetime.timezone.utc))
         else:
-            end_date = (datetime.datetime.strptime(os.getenv('END_DATE'), "%Y-%m-%d,%H:%M:%S")
+            if end_date is None:
+                self.stdout.write(self.style.ERROR('End date missing in .env.'))
+                sys.exit(1)
+            end_date = (datetime.datetime.strptime(end_date, "%Y-%m-%d,%H:%M:%S")
                         .replace(tzinfo=datetime.timezone.utc))
             end_date += datetime.timedelta(seconds=1)
         date_now = datetime.datetime.now(datetime.timezone.utc)
         delta = date_now - end_date
         earlier_day = datetime.datetime.now().replace(tzinfo=datetime.timezone.utc) - datetime.timedelta(days=1)
-        sensor_step = math.ceil(50 / (delta.days + 1) * 7)
-        measurements = []
-        for sensor_id_step in range(0, int(last_sensor_id / sensor_step) + 2):
-            ids_range = str(sensor_id_step * sensor_step + 1) + '-' + str((sensor_id_step + 1) * sensor_step)
+
+        sensor_range = math.ceil(50 / (delta.days + 1) * 7)
+        rows = []
+        for sensor_id_range in range(0, int(last_sensor_id / sensor_range) + 2):
+            ids_range = str(sensor_id_range * sensor_range + 1) + '-' + str((sensor_id_range + 1) * sensor_range)
             results = MeetJeStadAPIService().get_data(
                 end_date.strftime('%Y-%m-%d,%H:%M:%S'),
                 date_now.strftime('%Y-%m-%d,%H:%M:%S'),
@@ -49,44 +69,53 @@ class Command(BaseCommand):
         'json',
                 ids_range,
         False,
-                (delta.days + 1) * 24 * 4 * sensor_step,
+                (delta.days + 1) * 24 * 4 * sensor_range,
         False)
             if len(results) == 0:
                 continue
-            for row in results:
-                measurement = Measurement(row=row)
-                if measurement.sensor_id > last_sensor_id:
-                    last_sensor_id = measurement.sensor_id
-                if measurement.is_in_utrecht() and measurement.sensor_id not in sensors:
-                    measurements.append(measurement)
-                    sensor = Sensor()
-                    if measurement.pm25 is not None or measurement.pm10 is not None:
-                        sensor.is_particulate_matter = True
-                    else:
-                        sensor.is_particulate_matter = False
-                    if measurement.lux is not None:
-                        sensor.is_lux = True
-                    else:
-                        sensor.is_lux = False
-                    sensor.id = measurement.sensor_id
-                    sensor_repository.create(sensor)
-                    last_measurements[sensor.id] = measurement
-                elif measurement.sensor_id in sensors:
-                    sensor = sensors[measurement.sensor.id]
-                    if measurement.pm25 is not None or measurement.pm10 is not None:
-                        sensor.is_particulate_matter = True
-                    if measurement.lux is not None:
-                        sensor.is_lux = True
-                    measurements.append(measurement)
-                    last_measurements[sensor.id] = measurement
+            rows += results
+
+        measurements = []
+        last_sensor_id = 1
+        for row in rows:
+            measurement = Measurement(row=row)
+            if measurement.sensor_id > last_sensor_id:
+                last_sensor_id = measurement.sensor_id
+            if measurement.is_in_utrecht() and measurement.sensor_id not in sensors:
+                measurements.append(measurement)
+                sensor = Sensor()
+                if measurement.pm25 is not None or measurement.pm10 is not None:
+                    sensor.is_particulate_matter = True
+                else:
+                    sensor.is_particulate_matter = False
+                if measurement.lux is not None:
+                    sensor.is_lux = True
+                else:
+                    sensor.is_lux = False
+                sensor.id = measurement.sensor_id
+                sensor_repository.create(sensor)
+                last_measurements[sensor.id] = measurement
+            elif measurement.sensor_id in sensors:
+                sensor = sensors[measurement.sensor.id]
+                if measurement.pm25 is not None or measurement.pm10 is not None:
+                    sensor.is_particulate_matter = True
+                if measurement.lux is not None:
+                    sensor.is_lux = True
+                measurements.append(measurement)
+                last_measurements[sensor.id] = measurement
+
         measurement_repository.bulk_create(measurements)
+        sensor_repository.bulk_update(sensors)
+
         for measurement in measurements:
             if measurement.timestamp > earlier_day or measurement == last_measurements[measurement.sensor_id]:
                 measurements_cached.append(measurement)
-        sensor_repository.bulk_update(sensors)
         for measurement in measurements_cached:
             if measurement.timestamp < earlier_day and last_measurements[measurement.sensor.id].id != measurement.id:
                 measurements_cached.remove(measurement)
         measurement_cached_repository.write(measurements_cached)
+
         dotenv.set_key(dotenv_file, "LAST_SENSOR_ID", str(last_sensor_id), quote_mode='never')
         dotenv.set_key(dotenv_file, "END_DATE", date_now.strftime('%Y-%m-%d,%H:%M:%S'), quote_mode='never')
+
+        self.stdout.write(self.style.SUCCESS('Successfully updated dataset.'))
