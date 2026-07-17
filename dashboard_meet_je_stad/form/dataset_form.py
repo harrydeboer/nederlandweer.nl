9a11d5fe6063c1c9ee6da7c9921cce1b64dbf5a6
Harry de Boer <hldeboer@gmail.com>
2026-07-17 14:52:45 +0200

    dataset form update

--- dashboard_meet_je_stad/form/dataset_form.py
+++ dashboard_meet_je_stad/form/dataset_form.py
@@ -22,41 +22,43 @@ class DatasetForm(forms.Form):
 
     active_only = forms.BooleanField(initial=False, required=False, label='Alleen actieve sensors')
 
-    cutoff_temp = forms.BooleanField(initial=True, required=False, label='Afkap temperatuur')
+    cutoff_temp = forms.BooleanField(initial=CleanupService.cleanup_default['cutoff_temp']['is_on'],
+                                     required=False, label='Afkap temperatuur')
 
-    cutoff_temp_min = forms.FloatField(initial=CleanupService().cleanup_default['cutoff_temp']['min'],
+    cutoff_temp_min = forms.FloatField(initial=CleanupService.cleanup_default['cutoff_temp']['min'],
                                        required=False, label='Min temperatuur')
 
-    cutoff_temp_max = forms.FloatField(initial=CleanupService().cleanup_default['cutoff_temp']['max'],
+    cutoff_temp_max = forms.FloatField(initial=CleanupService.cleanup_default['cutoff_temp']['max'],
                                        required=False, label='Max temperatuur')
 
-    cutoff_pm25 = forms.BooleanField(initial=True, required=False, label='Afkap fijnstof 2.5')
+    cutoff_pm25 = forms.BooleanField(initial=CleanupService.cleanup_default['cutoff_pm25']['is_on'],
+                                     required=False, label='Afkap fijnstof 2.5')
 
-    cutoff_pm25_min = forms.FloatField(initial=CleanupService().cleanup_default['cutoff_pm25']['min'],
+    cutoff_pm25_min = forms.FloatField(initial=CleanupService.cleanup_default['cutoff_pm25']['min'],
                                        required=False, label='Min fijnstof 2.5')
 
-    cutoff_pm25_max = forms.FloatField(initial=CleanupService().cleanup_default['cutoff_pm25']['max'],
+    cutoff_pm25_max = forms.FloatField(initial=CleanupService.cleanup_default['cutoff_pm25']['max'],
                                        required=False, label='Max fijnstof 2.5')
 
-    cutoff_pm10 = forms.BooleanField(initial=True, required=False, label='Afkap fijnstof 10')
+    cutoff_pm10 = forms.BooleanField(initial=CleanupService.cleanup_default['cutoff_pm10']['is_on'],
+                                     required=False, label='Afkap fijnstof 10')
 
-    cutoff_pm10_min = forms.FloatField(initial=CleanupService().cleanup_default['cutoff_pm10']['min'],
+    cutoff_pm10_min = forms.FloatField(initial=CleanupService.cleanup_default['cutoff_pm10']['min'],
                                        required=False, label='Min fijnstof 10')
 
-    cutoff_pm10_max = forms.FloatField(initial=CleanupService().cleanup_default['cutoff_pm10']['max'],
+    cutoff_pm10_max = forms.FloatField(initial=CleanupService.cleanup_default['cutoff_pm10']['max'],
                                        required=False, label='Max fijnstof 10')
 
     def get_requested_cleanup(self) -> dict:
+        cleanup = CleanupService.cleanup_default
+        for key, value in CleanupService.cleanup_default.items():
+            for key_param, value_param in value.items():
+                if key_param == 'is_on':
+                    cleanup[key][key_param] = self[key].value()
+                else:
+                    cleanup[key][key_param] = self.set_value_to_number(self[key + '_' + key_param].value(), True)
 
-        return {'cutoff_temp': {'is_on': self['cutoff_temp'].value(),
-                         'min': self.set_value_to_number(self['cutoff_temp_min'].value(), True),
-                         'max': self.set_value_to_number(self['cutoff_temp_max'].value(), True)},
-         'cutoff_pm25': {'is_on': self['cutoff_pm25'].value(),
-                         'min': self.set_value_to_number(self['cutoff_pm25_min'].value(), True),
-                         'max': self.set_value_to_number(self['cutoff_pm25_max'].value(), True)},
-         'cutoff_pm10': {'is_on': self['cutoff_pm10'].value(),
-                         'min': self.set_value_to_number(self['cutoff_pm10_min'].value(), True),
-                         'max': self.set_value_to_number(self['cutoff_pm10_max'].value(), True)}}
+        return cleanup
 
     def set_value_to_number(self, value: str, return_float: bool) -> float|int|None:
         if value == '':
