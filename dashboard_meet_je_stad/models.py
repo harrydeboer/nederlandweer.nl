14425485cdaca955dd583b4112ea028a8f5938c0
Harry de Boer <hldeboer@gmail.com>
2026-07-18 10:20:11 +0200

    get and set for extra

--- dashboard_meet_je_stad/models.py
+++ dashboard_meet_je_stad/models.py
@@ -105,10 +105,13 @@ class Measurement(models.Model):
         self.battery = self.set_float(row[10])
         self.pm25 = self.set_float(row[11])
         self.pm10 = self.set_float(row[12])
-        if row[13] is None:
-            self.extra = 'null'
-        else:
-            self.extra = json.dumps(row[13])
+        self.set_extra(row[13])
+
+    def get_extra(self) -> str:
+        return json.loads(self.extra)
+
+    def set_extra(self, extra: list|None):
+        self.extra = json.dumps(extra)
 
     def is_in_utrecht(self) -> bool:
         utrecht_center_lat_degrees = 52.085 * math.pi / 180
@@ -143,7 +146,7 @@ class Measurement(models.Model):
                 if prop == 'pm2.5':
                     row.append(self.pm25)
                 elif prop == 'extra':
-                    row.append(json.loads(self.extra))
+                    row.append(self.get_extra())
                 else:
                     row.append(self.__getattribute__(prop))
         return row
