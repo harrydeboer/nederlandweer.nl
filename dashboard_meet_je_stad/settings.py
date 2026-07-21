e6ea9d98c2f800e0bd82f3d431a39bf1d74c5678
Harry de Boer <hldeboer@gmail.com>
2026-07-21 12:22:49 +0200

    domain added in settings

--- dashboard_meet_je_stad/settings.py
+++ dashboard_meet_je_stad/settings.py
@@ -38,7 +38,7 @@ if os.getenv('DEBUG') == '1':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'dashboard']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'meetjestadutrecht.nl']
 
 
 # Application definition
