9a2d60d59cfa3af52ee13d29dd9239f3c93cee05
Harry de Boer <hldeboer@gmail.com>
2026-07-22 17:04:05 +0200

    static root added

--- dashboard_meet_je_stad/settings.py
+++ dashboard_meet_je_stad/settings.py
@@ -101,6 +101,8 @@ DATABASES = {
     }
 }
 
+STATIC_ROOT = '/home/deb2005684/domains/meetjestadutrecht.nl/public_html/static'
+
 LOGIN_URL = '/inloggen'
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
