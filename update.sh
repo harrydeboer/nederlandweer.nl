28584e1efd4112d55e0e6ee1a7a4870ef44be7cf
Harry de Boer <hldeboer@gmail.com>
2026-07-24 10:47:10 +0200

    update.sh update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull origin main
-source .venv/bin/activate
+source /home/deb2005684/virtualenv/dashboard_meet_je_stad/3.13/bin/activate
 pip install -r requirements.txt
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 python manage.py migrate
