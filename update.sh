7b9dc7f6833c1fdbd5deaaa46138197deea71df8
Harry de Boer <hldeboer@gmail.com>
2026-07-28 19:46:46 +0200

    htaccess added

--- update.sh
+++ update.sh
@@ -4,3 +4,4 @@ source /home/deb2005684/virtualenv/dashboard_meet_je_stad/3.13/bin/activate
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+cp .htaccess ../public_html
