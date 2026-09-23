4e2eeb24f90c5e31ba62850ce67ca7e7229495a6
Harry de Boer <hldeboer@gmail.com>
2026-09-23 11:42:24 +0200

    update script update

--- update.sh
+++ update.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-set -e
-
-if [[ $EUID -eq 0 ]]; then
-  echo "This script must NOT be run as root" 1>&2
-  exit 1
-fi
-git pull origin master
+git pull origin main
 source .venv/bin/activate
-python manage.py migrate
 pip install -r requirements.txt
+python manage.py migrate
 service gunicorn restart
